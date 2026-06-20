3e8c975b6b66291344d962e1059292352840db1f
gaslac <crisgaslac741@gmail.com>
2026-06-20 16:39:05 -0500

    Actulizacion
    mejora para login de usario y detalles de las mascotas perdidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { RegistroMascota } from './pages/registro-mascota/registro-mascota';
 import { BuscarMascota } from './pages/buscar-mascota/buscar-mascota';
 import { DetalleMascota } from './pages/detalle-mascota/detalle-mascota';
 import { PanelSoporte } from './pages/panel-soporte/panel-soporte';
+import { Login } from './pages/login/login';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'registro', component: RegistroMascota },
-  { path: 'buscar', component: BuscarMascota },
+  { path: '',           component: Home },
+  { path: 'registro',   component: RegistroMascota },
+  { path: 'buscar',     component: BuscarMascota },
   { path: 'mascota/:id', component: DetalleMascota },
-  { path: 'soporte', component: PanelSoporte },
+  { path: 'login',      component: Login },
+  { path: 'soporte',    component: PanelSoporte, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
