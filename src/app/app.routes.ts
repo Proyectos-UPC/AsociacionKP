424b21a38494e4d62c12541451a30584d1c77667
gaslac <crisgaslac741@gmail.com>
2026-07-07 14:50:17 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,4 +15,3 @@ export const routes: Routes = [
   { path: 'login',      component: Login },
   { path: 'soporte',    component: PanelSoporte, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
-];
