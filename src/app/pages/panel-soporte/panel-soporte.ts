9affbb0b96b3898c52accee6efb6501793c9d465
Martinez-Pisfil Jorge-Luis <jorge.martinez-pisfil@inetum.com>
2026-07-12 13:07:07 -0500

    feat: implementar conexion con rest api de asociacion kp

--- src/app/pages/panel-soporte/panel-soporte.ts
+++ src/app/pages/panel-soporte/panel-soporte.ts
@@ -1,62 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MascotaService, Informe, Mascota, SolicitudBaja } from '../../services/mascota';
+import { MascotaService, Informe, SolicitudBaja } from '../../services/mascota';
 
 @Component({
   selector: 'app-panel-soporte',
   standalone: true,
-  imports: [CommonModule], // Agregado para permitir el uso de *ngIf y *ngFor en el HTML
-  templateUrl: './panel-soporte.html', 
+  imports: [CommonModule],
+  templateUrl: './panel-soporte.html',
   styleUrls: ['./panel-soporte.css']
 })
 export class PanelSoporte implements OnInit {
   informes: Informe[] = [];
-  mascotas: Mascota[] = [];
   solicitudesBaja: SolicitudBaja[] = [];
   cargando = true;
   accionExitosa: string | null = null;
 
-  constructor(private svc: MascotaService) {}
+  constructor(private svc: MascotaService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.cargar();
   }
 
   cargar() {
-    this.svc.listarMascotas().subscribe(m => { this.mascotas = m; });
-    this.svc.listarInformes().subscribe(i => { this.informes = i; this.cargando = false; });
-    this.svc.listarBajas().subscribe(b => { this.solicitudesBaja = b; });
+    this.svc.listarInformes().subscribe(i => {
+      this.informes = i;
+      this.cargando = false;
+      this.cdr.markForCheck();
+    });
+    this.svc.listarBajas().subscribe(b => {
+      this.solicitudesBaja = b;
+      this.cdr.markForCheck();
+    });
   }
-
-  getMascota(id: string) { return this.mascotas.find(m => m.id === id); }
 
   get pendientes()     { return this.informes.filter(i => i.estado === 'pendiente'); }
   get resueltos()      { return this.informes.filter(i => i.estado !== 'pendiente'); }
   get bajasPendientes(){ return this.solicitudesBaja.filter(b => b.estado === 'pendiente'); }
   get bajasResueltas() { return this.solicitudesBaja.filter(b => b.estado === 'procesado'); }
 
-  aprobar(mascotaId: string) {
-    this.svc.aprobarInforme(mascotaId).subscribe(() => {
-      this.accionExitosa = 'aprobado';
-      this.cargar();
-      setTimeout(() => this.accionExitosa = null, 2500);
-    });
+  private mostrarAccion(accion: string) {
+    this.accionExitosa = accion;
+    this.cargar();
+    this.cdr.markForCheck();
+    setTimeout(() => {
+      this.accionExitosa = null;
+      this.cdr.markForCheck();
+    }, 2500);
   }
 
-  rechazar(mascotaId: string) {
-    this.svc.rechazarInforme(mascotaId).subscribe(() => {
-      this.accionExitosa = 'rechazado';
-      this.cargar();
-      setTimeout(() => this.accionExitosa = null, 2500);
-    });
+  aprobar(informeId: string) {
+    this.svc.aprobarInforme(informeId).subscribe(() => this.mostrarAccion('aprobado'));
   }
 
-  procesarBaja(mascotaId: string) {
-    this.svc.procesarBaja(mascotaId).subscribe(() => {
-      this.accionExitosa = 'baja';
-      this.cargar();
-      setTimeout(() => this.accionExitosa = null, 2500);
-    });
+  rechazar(informeId: string) {
+    this.svc.rechazarInforme(informeId).subscribe(() => this.mostrarAccion('rechazado'));
+  }
+
+  procesarBaja(bajaId: string) {
+    this.svc.procesarBaja(bajaId).subscribe(() => this.mostrarAccion('baja'));
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://fl6jnvi9b4.execute-api.us-east-1.amazonaws.com',
+};
